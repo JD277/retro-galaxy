d3450ae3c153f5ebb58cbbffba63c118d8db5025
jdam <jdam5000@gmail.com>
2024-06-20 07:08:32 -0400

    Warning-window added

---
+++ Dimensions.md
@@ -0,0 +1,16 @@
+# Procedimiento para crear tu interfaz:
+Vas a abrir el archivo de ejemplo para ver los parametros que tienes que ponenerle a la clase luego te vas a ir a tu modulo y vas a instanciar esta clase despuès de haber importado los modulos `Interfaz` y `Button`
+
+#### OJO NO MODIFICAR LA CLASE INTERFAZ
+
+# Dimensiones para los elementos del juego
+- Las dimensiones de la ventana son 1080 de ancho y 720 de alto, 
+- las imagenes de los fondos deben tener las mismas medidas.
+
+- En cuanto a los inconos de los juegos deben ser de 400 X 400
+
+- La imagen de fondo para el warning debe ser de 800 x 300
+
+- El icono de la ventana warning debe ser de 128x128
+
+- El boton para cerrar la ventana warning debe ser de 32x32
--- modules/interface_example.py
+++ modules/interface_example.py
@@ -19,11 +19,11 @@ class Button_game:
         # text variables
         text_content = text
         if bold == 1:
-            text_font = pygame.font.Font("src/fonts/font1.otf", size)
+            text_font = pygame.font.Font("../retro-galaxy/src/fonts/font1.otf", size)
             self.TEXT_SURF = text_font.render(text_content, True, self.color1)
 
         elif bold == 0:
-            text_font = pygame.font.Font("src/fonts/font2.ttf", size)
+            text_font = pygame.font.Font("../retro-galaxy/src/fonts/font2.ttf", size)
             self.TEXT_SURF = text_font.render(text_content, True, self.color2)
         
         X_TEXT = pos_x
@@ -48,40 +48,55 @@ class Button_game:
         
 class Interface:
     # Define the constructor function for the class
-    def __init__(self, img, img_icon, x, y, title, description =["Lorem ipsum"], color1 =(255,255,255), color2=(255,255,255)):
+    def __init__(self, img, img_icon, x, y, title, description =["Lorem ipsum"], color1 =(255,255,255), color2=(255,255,255), wimg="../retro-galaxy/src/buttons/warning-btn.png", wicon="../retro-galaxy/src/buttons/clock-icon.png"):
+        
         # btn stuff
         self.btn = []
         btn_margin = 50
         self.btn_num = 5
         for i in range (self.btn_num):
-            self.btn.append(Button_game("./src/buttons/btn-white-m.png",x = btn_margin, y = 550))
+            self.btn.append(Button_game("../retro-galaxy/src/buttons/btn-white-m.png",x = btn_margin, y = 550))
             btn_margin += 200
+
+
         # Game icon
         X_POSGI = 600
         Y_POSGI = 150
         self.gicon_surf = pygame.image.load(img_icon)
         self.GI_RECT = pygame.Surface.get_rect(self.gicon_surf, topleft =(X_POSGI,Y_POSGI))
+
+
         # Text variables
         self.color1 = color1
         self.color2 = color2
         self.title = title
         self.des = description
+
+
         #Bg variables
         self.X_POS = x
         self.Y_POS = y
         self.bg_surf = pygame.image.load(img)
         self.BG_RECT = pygame.Surface.get_rect(self.bg_surf, topleft =(self.X_POS,self.Y_POS))
 
+
+        # Warning window vaiables
+        self.wimg = pygame.image.load(wimg).convert_alpha()
+        self.WRECT = pygame.Surface.get_rect(self.wimg,center = (540,360))
+        self.wicon =pygame.image.load(wicon).convert_alpha()
+        self.wirect = pygame.Surface.get_rect(self.wicon,center = (540,330))
+        self.wclose = Button_game("../retro-galaxy/src/buttons/cancel.png","",890,220)
+        self.wstate = False
     # Function to create dynamic text
     def CREATE_TEXT (self, size, pos_x, pos_y, text, bold=0,):
         # text variables
         text_content = text
         if bold == 1:
-            text_font = pygame.font.Font("src/fonts/font1.otf", size)
+            text_font = pygame.font.Font("../retro-galaxy/src/fonts/font1.otf", size)
             self.TEXT_SURF = text_font.render(text_content, True, self.color1)
 
         elif bold == 0:
-            text_font = pygame.font.Font("src/fonts/font2.ttf", size)
+            text_font = pygame.font.Font("../retro-galaxy/src/fonts/font2.ttf", size)
             self.TEXT_SURF = text_font.render(text_content, True, self.color2)
         
         X_TEXT = pos_x
@@ -89,15 +104,33 @@ class Interface:
         self.TEXT_RECT = pygame.Surface.get_rect(self.TEXT_SURF, topleft =(X_TEXT,Y_TEXT))
         screen.blit(self.TEXT_SURF, self.TEXT_RECT)
     
+    # Click event for soon btns
     def soon (self):
         for j in range(self.btn_num):
-            self.btn[j].btn_is_press()  
+            self.btn[j].btn_is_press()
             if self.btn[j].click:
+                self.wstate = True
                 return True
+            else: 
+                return False
+            
+    # Warning window
+    def window (self):
+        global screen
+        screen.blit(self.wimg,self.WRECT)
+        self.CREATE_TEXT(20,210,400,"Estamos  creando  más  juegos,  esta  atento a  las  actualizaciones :)",1)
+        screen.blit(self.wicon,self.wirect)
+        self.wclose.draw()
+
+    def wclose_warning(self):
+        self.wclose.btn_is_press()  
+        if self.wclose.click:
+            self.wstate = False
+
     # Draw the interface
     def draw(self):
         global screen
-        if self.soon() == False:
+        if self.soon() == False and self.wstate == False:
             screen.blit(self.bg_surf, self.BG_RECT)
             screen.blit(self.gicon_surf, self.GI_RECT)
             self.CREATE_TEXT(50,50,200, self.title,1)
@@ -107,7 +140,7 @@ class Interface:
                 value += 25
             for i in range(self.btn_num):
                 self.btn[i].draw()
-        else:
+        elif self.wstate:
             screen.blit(self.bg_surf, self.BG_RECT)
             screen.blit(self.gicon_surf, self.GI_RECT)
             self.CREATE_TEXT(50,50,200, self.title,1)
@@ -117,12 +150,14 @@ class Interface:
                 value += 25
             for i in range(self.btn_num):
                 self.btn[i].draw()
+            self.window()
+            self.wclose_warning()
 
 
 
 # Class arguments
-bg_image_path = "./src/backgrounds/travel-bg.jpeg"
-game_icon_path = "./src/sprites/navecita.png"
+bg_image_path = "../retro-galaxy/src/backgrounds/travel-bg.jpeg"
+game_icon_path = "../retro-galaxy/src/sprites/navecita.png"
 x = 0
 y = 0
 title = "Travel"
